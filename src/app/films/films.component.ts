2bb1ff17129a91f92c7737e6d20b8f753844474a
loic-msgb <loic.missigbeto@isen.yncrea.fr>
2025-03-31 11:16:26 +0200

    films component

--- src/app/films/films.component.ts
+++ src/app/films/films.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface Film {
+  id: string;
+  title: string;
+  original_title: string;
+  original_title_romanised: string;
+  image: string; // Ajout de la propriété image
+  description: string;
+  director: string;
+  producer: string;
+  release_date: string;
+  running_time: string;
+  rt_score: string;
+  people: string[];
+  species: string[];
+  locations: string[];
+  vehicles: string[];
+  url: string;
+}
 
 @Component({
   selector: 'app-films',
   imports: [],
   templateUrl: './films.component.html',
-  styleUrl: './films.component.css'
+  styleUrls: ['./films.component.css']
 })
-export class FilmsComponent {
+export class FilmsComponent implements OnInit {
+
+  films: Film[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get<Film[]>('https://ghibliapi.vercel.app/films')
+      .subscribe(data => {
+        this.films = data;
+      });
+  }
 
 }
