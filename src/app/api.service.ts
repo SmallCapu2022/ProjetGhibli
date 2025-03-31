52594cb643fa5f3d3cbb92b7f9ba7a5f60a3ab91
Capucine <debailleulcd@gmail.com>
2025-03-31 08:38:05 +0200

    test requete

---
+++ src/app/api.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+ 
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  httpClient = inject(HttpClient);
+ 
+  constructor(){}
+
+  getGhibliFilms(): Observable<any> {
+    return this.httpClient.get('https://ghibliapi.vercel.app/films')
+  }
+}
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ApiService } from './api.service';
+
 
 @Component({
   selector: 'app-root',
@@ -7,6 +9,12 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  apiService = inject(ApiService)
+  ngOnInit(): void {
+    this.apiService.getGhibliFilms().subscribe((response) => {
+      console.log(response);
+    });
+  }
   title = 'ProjetGhibli';
 }
