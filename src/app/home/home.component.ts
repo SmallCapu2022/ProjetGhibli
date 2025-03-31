d0cd0f36fd0a03ec0299539667cae098b76f33f3
Capucine <debailleulcd@gmail.com>
2025-03-31 15:17:16 +0200

    iteration 1 du quizz + favicon + logo dans navbar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FilmsService } from '../films/films.service';
+import { PeopleService } from '../people/people.service';
+import { signal, effect } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  private filmsService = inject(FilmsService);
+  private peopleService = inject(PeopleService);
 
+  films = signal<any[]>([]);  // Signal pour stocker les films
+  people = signal<any[]>([]); // Signal pour stocker les personnages
+
+  // Charger les films et les personnages au démarrage
+  constructor() {
+    effect(() => {
+      this.filmsService.getGhibliFilms().subscribe((films) => {
+        this.films.set(films);
+      });
+      this.peopleService.getGhibliPeople().subscribe((people) => {
+        this.people.set(people);
+      });
+    });
+  }
+
+  // Trouver les personnages d'un film particulier
+  getPeopleByFilm(filmId: string): any[] {
+    return this.people().filter((person) =>
+      person.films.includes(filmId)
+    );
+  }
+
+  // Créer les options de quiz pour chaque film
+  getQuizOptions(filmId: string): any[] {
+    const peopleInFilm = this.getPeopleByFilm(filmId);
+    const randomPerson = peopleInFilm[Math.floor(Math.random() * peopleInFilm.length)];
+
+    // Créer un tableau d'options (1 bonne et 3 fausses)
+    let options = [randomPerson];
+    while (options.length < 4) {
+      const randomOption = this.people()[Math.floor(Math.random() * this.people().length)];
+      if (!options.includes(randomOption)) {
+        options.push(randomOption);
+      }
+    }
+
+    // Mélanger les options pour avoir un ordre aléatoire
+    return options.sort(() => Math.random() - 0.5);
+  }
 }
